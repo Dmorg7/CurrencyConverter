8f34826da8339e87f53ad2ed417f481843725620
Benjamin Daniel Morgan <benjamindanielmorgan@benjamins-mbp.home>
2020-11-24 18:10:04 +0000

    first commit

---
+++ CurrencyConverter/Pages/Converter.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CurrencyConverter.Pages
+{
+    public class ConverterModel : PageModel
+    {
+		public string euro;
+        public string input;
+		public Double euroRate = 1.14;
+
+        public void OnGet()
+        {
+        }
+
+		public void OnPost()
+        {
+			string inputValue = Request.Form["input"];
+
+            if (!string.IsNullOrEmpty(inputValue))
+			{
+				double inputValueDouble = Convert.ToDouble(Request.Form["input"]);
+				double convertedValue = Math.Round(inputValueDouble * this.euroRate, 2);
+				this.euro = "Euros: €" + convertedValue.ToString("n2");
+			}
+
+
+			this.input = inputValue;
+        }
+    }
+}
